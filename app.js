fc2ccab7dc4822f921616ca6aac50c9c707ed30f
Xier <xier.ongzx@gmail.com>
2015-12-11 16:59:13 +0800

    changes

--- app.js
+++ app.js
@@ -30,10 +30,31 @@ app.use(validator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/', routes);
 app.use('/api/users', users);
 app.use('/api/units', units);
 app.use('/api/submissions', submissions);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
